92059d46e2878eeff6172ef3e23140cbc9f54829
maki-dai <rw.bskxmgc.3@gmail.com>
2024-02-05 00:07:45 +0900

    カレンダーレイアウト調整

--- resources/views/authenticated/calendar/admin/reserve_setting.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_setting.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.sidebar')
 @section('content')
-<div class="w-100 vh-100 d-flex" style="align-items:center; justify-content:center;">
-  <div class="w-100 vh-100 border p-5">
-    {!! $calendar->render() !!}
-    <div class="adjust-table-btn m-auto text-right">
+<div class=" pt-5 pb-5" style="background:#ECF1F6;">
+  <div class=" w-75 m-auto border pt-5 pb-5" style="border-radius:5px; background:#FFF;">
+    <div class="w-75 m-auto" style="border-radius:5px;">
+     <p class="text-center">{{ $calendar->getTitle() }}</p>
+     <p>{!! $calendar->render() !!}</p>
+     <div class="adjust-table-btn m-auto text-right">
       <input type="submit" class="btn btn-primary" value="登録" form="reserveSetting" onclick="return confirm('登録してよろしいですか？')">
-    </div>
+     </div>
+   </div>
   </div>
 </div>
 @endsection
